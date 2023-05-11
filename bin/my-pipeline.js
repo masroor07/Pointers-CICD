361865222529a24472c76c5d65df3ffb05638abd
masroor07 <2020a1r107@mietjammu.in>
2023-05-11 20:26:16 +0530

    Add initial

---
+++ bin/my-pipeline.js
@@ -0,0 +1,12 @@
+const cdk = require('aws-cdk-lib');
+const { MyPipelineStack } = require('../lib/my-pipeline-stack');
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '768392373232',
+    region: 'us-east-1',
+  }
+});
+
+app.synth();
